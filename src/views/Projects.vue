0e34da83abc25f946e5e314d7a08b132af570d84
Jan Hazincak <jan.hazincak@gmail.com>
2021-02-05 01:52:39 +0000

    Working on Home and Projects Page

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,5 +1,39 @@
 <template>
     <div>
-        <p>this is project page</p>
+        <div class="row justify-content-center">
+            <div class="col-md-6 text-center">
+                <h1><span>{{$t('my-projects')}}</span></h1>
+                <p class="lead">{{$t('projects-page-intro')}}</p>
+            </div>        
+        </div>
+        <hr>
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="row align-items-center">
+                    <div class="col-6">
+                        <img alt="project-thumbnail" class="img-fluid rounded img-thumbnail"  src="../assets/PhoneCasesShop.jpg">
+                    </div>
+                    <div class="col-6">
+                        <h4><span>PhoneCasesShop</span></h4>
+                        <p>{{$t('e-commerce-description')}}</p>
+                        <button class="btn btn-secondary btn-block">{{$t('more-button')}}</button>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-5">
+
+                    </div>
+                    <div class="col-7">
+                        <h4><span>Laptopmania</span></h4>
+                    </div>
+                </div>
+            </div>
+        </div>  
     </div>
+    
 </template>
